5572d4abc2ca2df9c43df7cdb72d9d2a8dfdfd36
Dan Anderson <89794369+Shingen47@users.noreply.github.com>
2023-08-28 13:04:24 +0800

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,18 @@
+from socket import *
+import time
+begin = time.time()
+
+if __name__ == "__main__":
+    addresstarget = input('enter host for scanning:')
+    t_IP = gethostbyname(addresstarget)
+    print('Scanning for open ports on : ', t_IP)
+
+    for i in range(50, 500):
+        soc = socket(AF_INET, SOCK_STREAM)
+        connectioncheck = s.connect_ex((t_IP, i))
+        if (connectioncheck == 0):
+            print('Port %d: OPEN' % (i,))
+        soc.close()
+print("The time taken for the scan:", time.time() - begin)
+
+
