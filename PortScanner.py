7849ba15f9ba50230338f75b3087d5ea232c611a
Dan Anderson <89794369+Shingen47@users.noreply.github.com>
2023-08-28 13:10:44 +0800

    PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     for i in range(50, 500):
         soc = socket(AF_INET, SOCK_STREAM)
-        connectioncheck = s.connect_ex((t_IP, i))
+        connectioncheck = soc.connect_ex((t_IP, i))
         if (connectioncheck == 0):
             print('Port %d: OPEN' % (i,))
         soc.close()
